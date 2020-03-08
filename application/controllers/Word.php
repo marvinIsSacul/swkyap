2ca892907bbb8087638ebd0052799fd3d2c11bbb
Marvin Kagiso <kagisomarvin@gmail.com>
2020-03-08 21:26:14 +0200

    Initial commit.

---
+++ application/controllers/Word.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Marvin Kagiso
+ * @since 09:24 2020/03/08
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * The Word 
+ */
+class Word extends CI_Controller {
+    public function index() {
+        echo 'hi';
+    }
+}
