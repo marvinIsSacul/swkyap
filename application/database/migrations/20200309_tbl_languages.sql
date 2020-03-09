10835b7239fbb980e93e34da6152edbb4e5ea437
Marvin Kagiso <kagisomarvin@gmail.com>
2020-03-09 11:10:27 +0200

    DB add languages.

--- application/database/migrations/20200309_tbl_languages.sql
+++ application/database/migrations/20200309_tbl_languages.sql
@@ -4,3 +4,29 @@ CREATE TABLE "tbl_languages" (
 	"iso_code"	TEXT,
 	"description"	TEXT
 );
+
+
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='1';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='2';
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='3';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='4';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='5';
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='5';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='6';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "id"=? WHERE "_rowid_"='7';
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='7';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='8';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='9';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='10';
+INSERT INTO "main"."tbl_languages"("id","name","iso_code","description") VALUES (NULL,'',NULL,NULL);
+UPDATE "main"."tbl_languages" SET "name"=? WHERE "_rowid_"='11';
